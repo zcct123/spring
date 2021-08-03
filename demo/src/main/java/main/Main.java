0ef443f835b4346251d68d2162b0163d680277f0
zclvct <1932335270@qq.com>
2021-08-03 13:02:42 +0800

    初始化

---
+++ demo/src/main/java/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import Config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import service.UserService;
+
+/**
+ * @description:
+ * @projectName:spring
+ * @see:main
+ * @author:赵冲
+ * @createTime:2021/8/3 12:26
+ * @version:1.0
+ */
+public class Main {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		UserService userService = (UserService)applicationContext.getBean("userService");
+		userService.test();
+	}
+}
---
+++ demo/src/main/java/service/UserService.java
@@ -0,0 +1,13 @@
+package service;
+
+/**
+ * @description:
+ * @projectName:spring
+ * @see:service
+ * @author:赵冲
+ * @createTime:2021/8/3 12:23
+ * @version:1.0
+ */
+public interface UserService {
+	void test();
+}
